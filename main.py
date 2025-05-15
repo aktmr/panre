1d0ce9b9214a563e522c84024feca3e07d166ff6
aktmr <145103468+aktmr@users.noreply.github.com>
2025-05-16 01:46:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -15,9 +15,6 @@ app = Flask(__name__)
 def home():
     return "Bot is alive!"
 
-def run_flask():
-    app.run(host='0.0.0.0', port=10000)
-
 flask_thread = threading.Thread(target=run_flask)
 flask_thread.daemon = True
 flask_thread.start()
